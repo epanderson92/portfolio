c32c16799a788379cc60d1b42673b1938b8f0b22
Evan Anderson <epanderson92@yahoo.com>
2017-01-19 00:23:30 -0600

    carousel, paralax, card 1, 2 and 3 added

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// carousel initialization
+   $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+      $(document).ready(function(){
+      $('.parallax').parallax();
+    });
